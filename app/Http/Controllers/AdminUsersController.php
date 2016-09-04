09074f521f69a5b875e3f56afc3cf336a138c69a
gaurav8k <gaurav8k@gmail.com>
2016-09-04 17:51:17 +0530

    user creation

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersRequest;
+use App\Role;
+use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -15,7 +18,8 @@ class AdminUsersController extends Controller
      */
     public function index()
     {
-        return view('admin.users.index');
+        $users = User::all();
+        return view('admin.users.index', compact('users'));
     }
 
     /**
@@ -25,7 +29,8 @@ class AdminUsersController extends Controller
      */
     public function create()
     {
-        return view('admin.users.create');
+        $roles = Role::lists('name','id')->all(); //We are pulling out specific data here.
+        return view('admin.users.create', compact('roles'));
     }
 
     /**
@@ -34,9 +39,11 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request) // This will go through UsersRequest, where we have added validation for input.
     {
-        //
+        /*return $request->all();*/
+        User::create($request->all());
+        return redirect('admin/users');
     }
 
     /**
